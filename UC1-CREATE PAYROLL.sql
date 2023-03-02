79d0eb4a55d944ea7033301b292becedccf81e7d
Rajeshwarikota <kotarajeswari003.com>
2023-03-02 16:28:27 +0530

    [Rajeshwari] Adding Employee table selection

--- UC1-CREATE PAYROLL.sql
+++ UC1-CREATE PAYROLL.sql
@@ -10,4 +10,10 @@ CREATE  TABLE Employee (
 );
 ---------UC3-INSERTION OF EMPLOYEE DETAILS-------------
 insert into Employee values ('Raji',450000,'2018-05-26')
+insert into Employee values ('Teju',350000,'2019-04-16')
+insert into Employee values ('Chandu',550000,'2023-07-12')
+insert into Employee values ('ammu',700000,'2022-08-09')
 insert into Employee (salary,Name,startDate) values (6000000,'swetha',getDate());
+---------UC4-SELECT FROM EMPLOYEE TABLE-----------------
+select *from Employee where Name = 'Raji';
+select *from Employee where Salary = 6000000;
