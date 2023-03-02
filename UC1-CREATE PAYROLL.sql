e45f0b3a244e5db9737247900ccc3cf098da45bb
Rajeshwarikota <kotarajeswari003.com>
2023-03-02 16:15:34 +0530

    [Rajeshwari] Adding Employee table INsertion

--- UC1-CREATE PAYROLL.sql
+++ UC1-CREATE PAYROLL.sql
@@ -1,4 +1,4 @@
-----------UC1-CRETE PAY ROLL SERVICE---------
+----------UC1-CRETE PAY ROLL SERVICE-------------------
 CREATE DATABASE PAYROLLSERVICE248;
 USE PAYROLLSERVICE248;
 ----------UC2-CREATE TABLE FOR PAY ROLL SERVICE--------
@@ -6,6 +6,8 @@ CREATE  TABLE Employee (
     Id int primary key identity(1,1),
     Name varchar(100),
     Salary float,
-    StartDate date,
-   
+    StartDate date, 
 );
+---------UC3-INSERTION OF EMPLOYEE DETAILS-------------
+insert into Employee values ('Raji',450000,'2018-05-26')
+insert into Employee (salary,Name,startDate) values (6000000,'swetha',getDate());
