5b02185d335e89e19cfe6af7f9d32c4f8c9958c0
Rajeshwarikota <kotarajeswari003.com>
2023-03-02 16:47:59 +0530

    [Rajeshwari] Adding Employee table selection with where conditon

--- UC1-CREATE PAYROLL.sql
+++ UC1-CREATE PAYROLL.sql
@@ -15,5 +15,8 @@ insert into Employee values ('Chandu',550000,'2023-07-12')
 insert into Employee values ('ammu',700000,'2022-08-09')
 insert into Employee (salary,Name,startDate) values (6000000,'swetha',getDate());
 ---------UC4-SELECT FROM EMPLOYEE TABLE-----------------
+select *from Employee 
 select *from Employee where Name = 'Raji';
 select *from Employee where Salary = 6000000;
+---------UC5-SELECT WITH WHERE QUERY--------------------
+select * from Employee WHERE StartDate BETWEEN CAST ('2018-0-01' as date ) and getDATE(); 
