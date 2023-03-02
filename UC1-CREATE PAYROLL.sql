1d52ec1cf352a644077fdc9931a5f58c9029674a
Rajeshwarikota <kotarajeswari003.com>
2023-03-02 15:57:56 +0530

    [Rajeshwari] Adding Employee table creation

--- UC1-CREATE PAYROLL.sql
+++ UC1-CREATE PAYROLL.sql
@@ -1,3 +1,11 @@
 ----------UC1-CRETE PAY ROLL SERVICE---------
 CREATE DATABASE PAYROLLSERVICE248;
 USE PAYROLLSERVICE248;
+----------UC2-CREATE TABLE FOR PAY ROLL SERVICE--------
+CREATE  TABLE Employee (
+    Id int primary key identity(1,1),
+    Name varchar(100),
+    Salary float,
+    StartDate date,
+   
+);
